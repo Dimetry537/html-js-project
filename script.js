ff4b738f7e4f684ed74873d9c6db0254ae5a24d9
dimetry537 <maksakov1988@gmail.com>
2024-09-12 19:45:49 +0300

    add styles and convert to sec_drd

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-///привет
+function loadComponent(url, elementId) {
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById(elementId).innerHTML = html;
+      })
+      .catch(error => {
+        console.error('Ошибка при загрузке компонента:', error);
+      });
+  }
+  
+  loadComponent('header.html', 'header');
+  
+  loadComponent('footer.html', 'footer');
